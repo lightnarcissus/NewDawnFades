94298b14f72e27f71e5d670fc09183f11bfd68ac
Ansh <lightnarcissus@gmail.com>
2015-10-28 12:46:51 -0400

    Created simple scene with movement and background

---
+++ NewDawnFades/Assets/MovePlayer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	private Animator playerAnim;
+	public GameObject forest;
+	// Use this for initialization
+	void Start () {
+	
+		playerAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKey (KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+		{
+			transform.position+=new Vector3(-0.01f,0f,0f);
+		}
+		else if(Input.GetKey (KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+		{
+			transform.position+=new Vector3(0.0001f,0f,0f);
+			forest.GetComponent<Renderer>().material.mainTextureOffset+=new Vector2(0.001f,0f);
+			playerAnim.SetBool("WalkRight",true);
+			playerAnim.Play("WalkRight");
+		}
+		else if(Input.GetKey (KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position+=new Vector3(0f,0.01f,0f);
+		}
+		else if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) {
+			transform.position += new Vector3 (0f, -0.01f, 0f);
+		} else {
+			playerAnim.Play("Idle");
+		}
+	
+	
+	}
+}
