0728866320c381b3e03a0ae240bc6c72fc868b43
Ansh <lightnarcissus@gmail.com>
2016-05-27 18:28:28 -0400

    Fixed read text on consistent freq and music

--- NewDawnFades/Assets/Scripts/ReadText.cs
+++ NewDawnFades/Assets/Scripts/ReadText.cs
@@ -2,12 +2,12 @@
 using System;
 using System.Collections;
 using System.IO;
-
+using System.Collections.Generic;
 
 public class ReadText : MonoBehaviour {
-	StreamReader sr = new StreamReader("music.txt");
+//	StreamReader sr = new StreamReader("music.txt");
+  //  StreamReader fq = new StreamReader("freq.txt");
 	public static string str;
-	public string freqString;
 	public static float num;
 	public static float freqNum;
 	public GameObject renderQuad;
@@ -15,11 +15,19 @@ public class ReadText : MonoBehaviour {
 	public GameObject text;
 	public static bool close=false;
 	public GameObject dirLight;
+    public TextAsset music;
+    public TextAsset freq;
     private string pathName = "music.txt";
     private string freqPathName = "freq.txt";
+    private int musicCount = 0;
+    private int freqCount = 0;
+    private string[] musicString;
+    private string[] freqString;
 	// Use this for initialization
 	void Start () {
 		renderQuad.SetActive (false);
+        musicString = music.text.Split("\n"[0]);
+        freqString=freq.text.Split("\n"[0]);
 		//str = sr.ReadLine ();
 		//str = sr.();
 		//sr.Close();    
@@ -42,17 +50,23 @@ public class ReadText : MonoBehaviour {
 		if (timer > 290f) {
 			Application.Quit();
 		}
-		StreamReader sr = new StreamReader(pathName);
-        StreamReader fq = new StreamReader(freqPathName);
-		str = sr.ReadLine();
-		freqString = fq.ReadLine ();
+     /*   sr = new StreamReader(pathName);
+        fq = new StreamReader(freqPathName);
+        	str = sr.ReadLine();
+        freqString = fq.ReadLine ();
+        */
 	}
 	void LateUpdate () {
-		//Console.WriteLine(str);
+        //Console.WriteLine(str);
 
-		//Debug.Log (str);
-		float.TryParse(str,out num);
-		float.TryParse(freqString,out freqNum);
+        //Debug.Log (str);
+        float.TryParse(freqString[freqCount], out freqNum);
+        float.TryParse(musicString[musicCount], out num);
+        freqCount++;
+        musicCount++;
+        //float.TryParse(str,out num);
+		//float.TryParse(freqString,out freqNum);
 		//Debug.Log (num);
+       // Debug.Log(freqNum);
 	}
 }
